8d37cd033dd1ddf67d4b23632e543ddbdbb432ee
r3d1bruh <cruxn@outlook.com>
2026-04-11 03:22:56 +0600

    Track the build directory for GitHub Pages deploy

---
+++ build/precache-manifest.0a9f84cbcb72287895a00dd626fdf4aa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a3f6f4ffb1e815e4c20be1a3dca7a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b43c5572547eb830f42",
+    "url": "./static/css/main.1678b5f1.chunk.css"
+  },
+  {
+    "revision": "5d32d6e1734fd2c5184d",
+    "url": "./static/js/2.20afe037.chunk.js"
+  },
+  {
+    "revision": "6b43c5572547eb830f42",
+    "url": "./static/js/main.2b98ad29.chunk.js"
+  },
+  {
+    "revision": "14385458bde04e4c3c47",
+    "url": "./static/js/runtime~main.f2f1b961.js"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "./static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "./static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "./static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "./static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "./static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "./static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "33211a17eb64aaa6b94254b72a70bcb9",
+    "url": "./static/media/smile.33211a17.svg"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "./static/media/view-info.415b027d.ico"
+  }
+]);
